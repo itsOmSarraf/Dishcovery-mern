72167712e29aaf86b83c19cedb2fcb262b299fb2
Om Sarraf <itsomsarraf+github@gmail.com>
2024-02-24 17:14:04 +0530

    cleark setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
+import {
+	SignOutButton,
+	SignInButton,
+	SignedIn,
+	SignedOut
+} from '@clerk/clerk-react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div>
+			<SignedOut>
+				<SignInButton />
+				<p>
+					This content is public. Only signed out users can see the SignInButton
+					above this text.
+				</p>
+			</SignedOut>
+			<SignedIn>
+				<SignOutButton signOutCallback={() => redirect('/')} />
+				<p>
+					This content is private. Only signed in users can see the
+					SignOutButton above this text.
+				</p>
+			</SignedIn>
+		</div>
+	);
 }
 
-export default App
+export default App;
