393a2f7d123471b59a412f49d34de2cc5ec73716
Om Sarraf <itsomsarraf+github@gmail.com>
2024-03-12 19:57:38 +0530

    loader added

---
+++ frontend/src/components/loading.jsx
@@ -0,0 +1,11 @@
+export default function Loader() {
+	return (
+		<div className='loading w-[500px] h-[500px]'>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	);
+}
