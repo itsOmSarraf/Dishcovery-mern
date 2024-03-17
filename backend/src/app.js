9ade69fca13bb77e975537f82f15a5a5895f1914
Om Sarraf <itsomsarraf+github@gmail.com>
2024-03-17 13:39:43 +0530

    random

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+
+// Import routes
+import healthCheckRoute from './routes/healtcheck.routes.js';
+import recipeRoute from './routes/recipe.route.js';
+import geminiRoute from './routes/gemini.route.js';
+
 const app = express();
 
+// Middleware
 app.use(cors());
 const jsonParser = bodyParser.json({ limit: '10mb' });
 const urlencodedParser = bodyParser.urlencoded({
@@ -12,13 +19,14 @@ const urlencodedParser = bodyParser.urlencoded({
 app.use(express.json());
 app.use(jsonParser);
 app.use(urlencodedParser);
-// app.use(express.json({ limit: '100mb' }));
-// app.use(express.raw({ limit: '100mb', type: 'image/*' }));
 
-import healthCheckRoute from './routes/healtcheck.routes.js';
-import recipeRoute from './routes/recipe.route.js';
-import geminiRoute from './routes/gemini.route.js';
+// Add CORS middleware to set Access-Control-Allow-Origin header
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	next();
+});
 
+// Routes
 app.use('/api/v1/healthcheck', healthCheckRoute);
 app.use('/api/v1/recipes', recipeRoute);
 app.use('/api/v1/upload', geminiRoute);
